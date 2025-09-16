fe36ce9be744c51b1e3435b4be8a19064481abfd
Martin Stavro (Madison Springfield) <martin.stavro@madisonspringfield.com>
2025-09-16 11:59:03 -0400

    Stretch tasks and update theme to bootswatch zephyr

--- app.R
+++ app.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(bslib)
+library(shinyvalidate)
 
 generate_story <- function(noun, verb, adjective, adverb) {
   glue::glue(
@@ -10,16 +12,20 @@ generate_story <- function(noun, verb, adjective, adverb) {
 }
 
 ui <- fluidPage(
+  theme = bs_theme(bootswatch = "zephyr"),
   titlePanel("Mad Libs Game"),
+  p(
+    "Fill in the boxes below, and this app will create a fun statement for you! Watch your statement update in real time as you type in the boxes."
+  ),
   sidebarLayout(
     sidebarPanel(
       textInput("noun1", "Enter a noun:", ""),
       textInput("verb", "Enter a verb:", ""),
       textInput("adjective", "Enter an adjective:", ""),
-      textInput("adverb", "Enter an adverb:", ""),
-      actionButton("submit", "Create Story")
+      textInput("adverb", "Enter an adverb:", "")
     ),
     mainPanel(
+      p(""),
       h3("Your Mad Libs Story:"),
       textOutput("story")
     )
@@ -27,12 +33,18 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  story <- eventReactive(input$submit, {
+  iv <- InputValidator$new()
+  iv$add_rule("noun1", sv_required())
+  iv$add_rule("verb", sv_required())
+  iv$add_rule("adjective", sv_required())
+  iv$add_rule("adverb", sv_required())
+  story <- reactive({
     generate_story(input$noun1, input$verb, input$adjective, input$adverb)
   })
   output$story <- renderText({
     story()
   })
+  iv$enable()
 }
 
 shinyApp(ui = ui, server = server)
